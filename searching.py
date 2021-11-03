132a11317862f2bcf4db5f76b968028b108a4b8e
cbarb001 <cbarb001@gmail.com>
2021-11-02 17:57:25 -0700

    Practice while loop

--- searching.py
+++ searching.py
@@ -1,12 +1,15 @@
 shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 
-item_to_find = "albatross"
+item_to_find = "spam"
 found_at = None
 
-for index in range(len(shopping_list)):
-    if shopping_list[index] == item_to_find:
-        found_at = index
-        break
+# for index in range(len(shopping_list)):
+#     if shopping_list[index] == item_to_find:
+#         found_at = index
+#         break
+
+if item_to_find in shopping_list:
+    found_at = shopping_list.index(item_to_find)
 
 if found_at is not None:
     print("Item found at position {}.".format(found_at))
