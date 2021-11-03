05ff18b5c2d0b3a36a90e05c1095c2e3b8a5117c
cbarb001 <cbarb001@gmail.com>
2021-11-02 17:38:53 -0700

    Test break in searching.py

--- searching.py
+++ searching.py
@@ -1,11 +1,14 @@
 shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 
-item_to_find = "spam"
+item_to_find = "albatross"
 found_at = None
 
-for index in range(0, 6):
-    if shopping_list[index] == "spam":
+for index in range(len(shopping_list)):
+    if shopping_list[index] == item_to_find:
         found_at = index
         break
 
-print(found_at)
+if found_at is not None:
+    print("Item found at position {}.".format(found_at))
+else:
+    print("{} not found in shopping list.".format(item_to_find))
--- shopping.py
+++ shopping.py
@@ -2,6 +2,6 @@ shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 
 for item in shopping_list:
     if item == "spam":
-        break
+        continue
 
     print("Buy " + item)
